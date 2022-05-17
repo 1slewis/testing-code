7f7d82dcec74cbfd3e3d4ffc902003e4e6a642c9
1slewis <77329743+1slewis@users.noreply.github.com>
2022-05-17 10:43:09 +0100

    Update Solution.java

--- RemoveMultipleValues/Solution.java
+++ RemoveMultipleValues/Solution.java
@@ -1,24 +1,32 @@
 import java.util.ArrayList;
 
 public class Solution {
-    public static int[] solution(int[] data, int n) {
-        // Your code here
-        ArrayList<Integer> outputArrayList = new ArrayList<Integer>();
-        for (int i : data) {
-            int amount = 0;
+    public static int count(int[] data, int i) {
+        int amount = 0;
             for (int j : data) {
                 if (i == j) {
                     amount++;
                 }
             }
+        return amount;
+    }
+    public static int[] ArrayList2intArray(ArrayList<Integer> inputArrayList) {
+        int[] outputintArray = new int[inputArrayList.size()];
+        for (int i = 0; i < inputArrayList.size(); i++) {
+            outputintArray[i] = inputArrayList.get(i);
+        }
+        return outputintArray;
+    } 
+    public static int[] solution(int[] data, int n) {
+        // Your code here
+        ArrayList<Integer> outputArrayList = new ArrayList<Integer>();
+        for (int i : data) {
+            int amount = Solution.count(data, i);
             if (amount <= n) {
                 outputArrayList.add(i);
             }
         }
-        int[] output = new int[outputArrayList.size()];
-        for (int i = 0; i < outputArrayList.size(); i++) {
-            output[i] = outputArrayList.get(i);
-        }
+        int[] output = ArrayList2intArray(outputArrayList);
         return output;
     }
 }
