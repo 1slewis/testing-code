c2cf00a67be42a57a3300e6ad68179fed06835a4
1slewis <77329743+1slewis@users.noreply.github.com>
2022-05-17 22:49:52 +0100

    Update Solution.java

--- LocationToID/Solution.java
+++ LocationToID/Solution.java
@@ -1,1 +1,12 @@
-
+public class Solution {
+    public static String solution(long x, long y) {
+        //Your code here
+        int n = ((int)x + (int)y - 2);
+        int result = (n * (n + 1)) / 2;
+        return Integer.toString(result + (int)x);
+    }
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(5, 10)); // 96
+        System.out.println(Solution.solution(3, 2)); // 9
+    }
+}
